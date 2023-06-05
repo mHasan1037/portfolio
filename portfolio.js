f91ee60e5b9e876baed2455a0f34212d9de1431b
Mahmudul Hasan <mhasan.bd.95@gmail.com>
2023-06-05 22:18:23 +0600

    added about and skill section to the portfolio

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,26 @@
+//buger click on the nav
+const menuBtn = document.getElementById('burger-menu')
+const navItems = document.getElementById('nav-items');
+const burgerIcon = document.querySelector('.fa-solid.fa-bars')
+const navCloseIcon = document.querySelector('.fa-solid.fa-xmark')
+
+let menuOpen = false;
+
+menuBtn.addEventListener('click', ()=>{
+   navItems.classList.toggle('showNav')
+   menuOpen = !menuOpen;
+
+   if(menuOpen){
+    burgerIcon.style.display = 'none';
+    navCloseIcon.style.display = 'block';
+    } else {
+        burgerIcon.style.display = 'block';
+        navCloseIcon.style.display = 'none';
+    }
+
+})
+
+// typewritter effect text change
 const text = document.querySelector('.sec-text');
 
 const textLoad = () =>{

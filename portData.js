e69299d4000a18b1392185ac3ce07d74672a11e0
Mahmudul Hasan <mhasan.bd.95@gmail.com>
2023-06-06 19:42:07 +0600

    completed the portfolio design

---
+++ portData.js
@@ -0,0 +1,89 @@
+export let portfolioData = [
+    {
+        id: 1,
+        type: 'react',
+        name: 'React Dashboard',
+        webLink: 'https://mhasan1037.github.io/react-dashboard/',
+        codeLink: 'https://github.com/mHasan1037/react-dashboard',
+        img: './Image/dashboard.png',
+        techs: ['React JS']
+    },
+    {
+        id: 2,
+        type: 'frontEnd',
+        name: 'Ecommerce website',
+        webLink: 'https://mhasan1037.github.io/E-commerce-2/',
+        codeLink: 'https://github.com/mHasan1037/E-commerce-2',
+        img: './Image/port1.JPG',
+        techs: ["HTML", "CSS", "JS"]
+    },
+    {
+        id: 3,
+        type: 'react',
+        name: 'Stock-app',
+        webLink: 'https://mhasan1037.github.io/stock-app/',
+        codeLink: 'https://github.com/mHasan1037/stock-app',
+        img: './Image/stock-app.JPG',
+        techs: ["React JS", "SCSS"]
+    },
+    {
+        id: 4,
+        type: 'react',
+        name: 'Ecommerce website',
+        webLink: 'https://mhasan1037.github.io/e-commerce/',
+        codeLink: 'https://github.com/mHasan1037/e-commerce',
+        img: './Image/ecommer-2.JPG',
+        techs: ["React JS"] 
+    },
+    {
+        id: 5,
+        type: 'react',
+        name: 'Expense-Tracker',
+        webLink: 'https://mhasan1037.github.io/expense-tracker/',
+        codeLink: 'https://github.com/mHasan1037/expense-tracker',
+        img: './Image/expense-tracker.JPG',
+        techs: ["React JS"]
+    },
+    {
+        id: 6,
+        type: 'frontEnd',
+        name: 'Construction website',
+        webLink: 'https://mhasan1037.github.io/Construction-Website/',
+        codeLink: 'https://github.com/mHasan1037/Construction-Website',
+        img: './Image/Construction.JPG',
+        techs: ['HTML', "CSS", 'JS']
+    },
+    {
+        id: 7,
+        type: 'frontEnd',
+        name: 'Ecommerce website',
+        webLink: 'https://mhasan1037.github.io/Complete-e-commerce/',
+        codeLink: 'https://github.com/mHasan1037/Complete-e-commerce',
+        img: './Image/port3.JPG',
+        techs: ["HTML", "CSS", "JS"]
+    },
+    {
+        id: 8,
+        type: 'others',
+        name: 'PSD-To-HTML',
+        webLink: 'https://mhasan1037.github.io/PSD-to-HTML-1/',
+        codeLink: 'https://github.com/mHasan1037/PSD-to-HTML-1',
+        img: './Image/Interior.JPG',
+        techs: ['HTML', 'CSS', 'JS']
+    },
+    {
+        id: 9,
+        type: 'others',
+        name: 'Figma-To-HTML',
+        webLink: 'https://mhasan1037.github.io/Figma-to-HTML-1/',
+        codeLink: 'https://github.com/mHasan1037/Figma-to-HTML-1',
+        img: './Image/OXPAY.JPG',
+        techs: ['HTML', 'CSS', 'JS']
+    }
+]
+
+
+
+
+
+
--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,5 @@
+import { portfolioData } from './portData.js';
+
 //buger click on the nav
 const menuBtn = document.getElementById('burger-menu')
 const navItems = document.getElementById('nav-items');
@@ -42,3 +44,101 @@ textLoad()
 setInterval(()=>{
     textLoad()
 }, 12000)
+
+
+//displaying all portfolio data in the html page
+const portContainer = document.querySelector('.portfolio-container')
+
+portfolioData.slice(0, 6).forEach((port, idx)=>{
+    const {id, name, type, webLink, codeLink, img, techs} = port
+   
+    const box = document.createElement('div')
+    box.innerHTML = `
+            <div class="portfolio-box" id=${codeLink}>
+                    <img class="portImg" src=${img} />
+                    <div class="portOverlay">
+                        <i class="fa-regular fa-eye"></i>
+                        <div class="port-textBox">
+                            <h3>${name}</h3>
+                            <p>
+                              ${techs.map((tech, idx) => tech)}
+                            </p>
+                        </div>
+                    </div>
+            </div>
+    `
+
+    portContainer.appendChild(box)
+})
+
+
+// on click of the portfolio box you will see the data in the modal
+const portIds = document.querySelectorAll('.portfolio-box')
+
+portIds.forEach((ids, idx)=>{
+    ids.addEventListener('click', ()=>{
+        getModal(ids.id)
+    })
+})
+
+function getModal(idIs){
+    const res = portfolioData.filter((data) => data.codeLink === idIs)
+
+    const {id,img, name, type, webLink, codeLink, techs} = res[0]
+
+    const modalContainer = document.querySelector('.modal-container')
+    modalContainer.classList.add('modal-visible')
+
+    modalContainer.innerHTML = `
+        <div class="modal-box">
+                <div class="close-modal">
+                    <i class="fa-solid fa-xmark"></i>
+                </div>
+                <h1>${name}</h1>
+                <p>This website is made for my portfolio only.</p>
+                <div class="modal-options">
+                    <div>
+                        <i class="fa-brands fa-windows"></i>
+                        <p>Website</p>
+                    </div>
+                    <div>
+                        <i class="fa-solid fa-link"></i>
+                        <a href=${webLink} target="_blank">Source code</a>
+                    </div>
+                    <div>
+                        <i class="fa-brands fa-codepen"></i>
+                        <p>${techs.map(tech => tech)}</p>
+                    </div>
+                    <div>
+                        <i class="fa-solid fa-tv"></i>
+                        <a href=${codeLink}>Live Preview</a>
+                    </div>
+                </div>
+
+                <img src=${img} />
+        </div>
+    `
+
+    
+
+    const closeModal = document.querySelector('.close-modal')
+    closeModal.addEventListener('click', ()=>{
+        modalContainer.classList.remove('modal-visible')
+    })
+
+    console.log(id)
+    console.log(res)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
